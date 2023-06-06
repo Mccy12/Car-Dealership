3bf9355eff83eafa5256030e163b4107e1068a1c
Joshua <contrerasjoshua@hotmail.com>
2023-06-06 12:04:00 -0700

    Fixed Auto List

--- ghi/app/src/AutomobileList.js
+++ ghi/app/src/AutomobileList.js
@@ -27,22 +27,24 @@ class AutomobileList extends React.Component {
                 <table className="table">
                     <thead>
                         <tr>
+                            <th>VIN</th>
                             <th>Color</th>
                             <th>Year</th>
-                            <th>Vin</th>
                             <th>Model</th>
-                            <th>Make</th>
+                            <th>Manufacturer</th>
+                            <th>Sold</th>
                         </tr>
                     </thead>
                     <tbody>
                         {this.state.autos.map(auto => {
                             return (
                                 <tr key={auto.id}>
+                                    <td>{auto.vin}</td>
                                     <td>{auto.color}</td>
                                     <td>{auto.year}</td>
-                                    <td>{auto.vin}</td>
                                     <td>{auto.model.name}</td>
                                     <td>{auto.model.manufacturer.name}</td>
+                                    <td>{auto.sold ? 'Yes' : 'No'}</td>
                                 </tr>
                             );
                         })}
