5cd75e2616b16d332bcc6f7e7cfca5b6597b93ec
Matthew Haffke <haffkematt@gmail.com>
2023-06-06 15:17:34 -0700

    Made scaffolding for services for React stuff

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -10,11 +10,13 @@ function Nav() {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li>
-            <NavLink className="nav-link" aria-current="page" to='/models'>Models</NavLink>
+            <li className="nav-item">
+              <NavLink className="nav-link" aria-current="page" to='/models'>
+                Models
+              </NavLink>
             </li>
-            <li>
-            <NavLink className="nav-link" aria-current="page" to='/models/new'>Create a Model</NavLink>
+            <li className="nav-item">
+              <NavLink className="nav-link" aria-current="page" to='/models/new'>Create a Model</NavLink>
             </li>
             <li className="nav-item">
                 <NavLink className="nav-link" aria-current="page" to="/manufacturers">
