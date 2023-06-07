050cd830d9cf29aaa180b6cbafa714ba185e2e27
Matthew Haffke <haffkematt@gmail.com>
2023-06-07 13:50:21 -0700

    Fixed navbar to wrap. It's flex-wrap in the ul

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -10,7 +10,7 @@ function Nav() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 flex-wrap">
             <li className="nav-item">
               <NavLink className="nav-link" aria-current="page" to='/models'>
                 Models
