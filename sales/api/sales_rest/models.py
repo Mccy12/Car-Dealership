ddef175617f71b904081f4ee11d3e3e007df594a
Joshua <contrerasjoshua@hotmail.com>
2023-06-06 18:05:09 -0700

    SalesModelViews

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=50, unique=True)
+    sold = models.BooleanField(default=False)
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=50)
+
+class SalesPerson(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    employee_id = models.CharField(max_length=50)
+
+class Sale(models.Model):
+
+    automobile = models.ForeignKey(
+        AutomobileVO,
+        related_name="automobiles",
+        on_delete=models.PROTECT
+    )
+    sales_person = models.ForeignKey(
+        SalesPerson,
+        related_name="sales",
+        on_delete=models.PROTECT
+    )
+    customer = models.ForeignKey(
+        Customer,
+        related_name="customers",
+        on_delete=models.PROTECT
+    )
+    price = models.CharField(max_length=100)
+
+#    def get_api_url(self):
+ #       return reverse("api_show_sales", kwargs={"pk": self.pk})
---
+++ sales/api/sales_rest/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import (
+    api_list_sale,
+    api_list_salesperson,
+    api_list_customer,
+    api_show_salesperson,
+    api_show_sale,
+    api_show_customer
+)
+
+urlpatterns = [
+    path('sales/', api_list_sale, name="api_list_sale"),
+    path('salespeople/', api_list_salesperson, name="api_list_salesperson"),
+    path("customers/", api_list_customer, name="api_list_customer"),
+    path("salespeople/<int:id>/", api_show_salesperson, name="api_show_salesperson"),
+    path("sales/<int:id>/", api_show_sale, name="api_show_sale"),
+    path("customers/<int:id>/", api_show_customer, name="api_show_customer")
+]
